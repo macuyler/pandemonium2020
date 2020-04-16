413635209435973ffa59f61b8e863f77991390b2
Macuyler <macuyler@gmail.com>
2020-04-15 22:47:18 -0500

    Update colors

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-const List<Color> leftColors = [Colors.red, Colors.orange, Colors.yellow, Colors.green, Colors.lightBlueAccent];
-const List<Color> rightColors = [Colors.indigoAccent, Colors.purple, Colors.pink, Color.fromRGBO(227, 107, 195, 1), Colors.lightGreenAccent];
+const List<Color> leftColors = [Color.fromRGBO(255, 0, 0, 1), Colors.orange, Colors.yellow, Color.fromRGBO(22, 102, 10, 1), Colors.lightBlueAccent];
+const List<Color> rightColors = [Color.fromRGBO(5, 56, 159, 1), Color.fromRGBO(147, 44, 160, 1), Color.fromRGBO(243, 177, 234, 1), Color.fromRGBO(134, 255, 217, 1), Color.fromRGBO(134, 134, 134, 1)];
 
 const double blockWidth = 45;
 const double blockHeight = 45;

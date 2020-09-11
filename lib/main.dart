b7a03355abe220578a1312c7bf7bf3557362f1e0
Macuyler <macuyler@gmail.com>
2020-09-10 19:10:03 -0500

    Use comic font for splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -35,60 +35,73 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return _showLevels ? LevelScreen() : Scaffold(
-      backgroundColor: Color.fromRGBO(25, 25, 25, 1),
-      appBar: getStatusBar(Brightness.dark),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Pandemonium',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28
-              ),
-            ),
-            Text('2020',
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 70
-              ),
-            ),
-            OutlineButton(
-              color: Colors.white,
-              splashColor: Color.fromRGBO(255, 255, 255, 0.6),
-              highlightColor: Color.fromRGBO(255, 255, 255, 0.6),
-              borderSide: BorderSide(
-                color: Color.fromRGBO(255, 255, 255, 0.8),
-                width: 1.0,
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(35),
-              ),
-              child: SizedBox(
-                width: 160,
-                height: 35,
-                child: Center(
-                  child: Text('START',
-                    textAlign: TextAlign.center,
+    return _showLevels
+        ? LevelScreen()
+        : Scaffold(
+            backgroundColor: Color.fromRGBO(25, 25, 25, 1),
+            appBar: getStatusBar(Brightness.dark),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Pandemonium',
                     style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 22
-                    ),
+                        color: Color.fromRGBO(254, 209, 40, 1),
+                        fontSize: 28,
+                        shadows: [
+                          Shadow(
+                              blurRadius: 1.0,
+                              color: Color.fromRGBO(250, 28, 22, 1),
+                              offset: Offset(1.6, 1.6))
+                        ]),
                   ),
-                )
+                  Text(
+                    '2020',
+                    style: TextStyle(
+                        color: Color.fromRGBO(254, 209, 40, 1),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 70,
+                        shadows: [
+                          Shadow(
+                              blurRadius: 1.0,
+                              color: Color.fromRGBO(250, 28, 22, 1),
+                              offset: Offset(3.0, 3.0))
+                        ]),
+                  ),
+                  OutlineButton(
+                    color: Colors.white,
+                    splashColor: Color.fromRGBO(255, 255, 255, 0.6),
+                    highlightColor: Color.fromRGBO(255, 255, 255, 0.6),
+                    borderSide: BorderSide(
+                      color: Color.fromRGBO(255, 255, 255, 0.8),
+                      width: 1.0,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(35),
+                    ),
+                    child: SizedBox(
+                        width: 160,
+                        height: 35,
+                        child: Center(
+                          child: Text(
+                            'START',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 22),
+                          ),
+                        )),
+                    onPressed: () {
+                      setState(() {
+                        _showLevels = true;
+                      });
+                    },
+                  ),
+                ],
               ),
-              onPressed: () {
-                setState(() {
-                  _showLevels = true;
-                });
-              },
             ),
-          ],
-        ),
-      ),
-    );
+          );
   }
 }

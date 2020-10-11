9dd19045bcb56554182916a48bf01b243031616d
Macuyler <macuyler@gmail.com>
2020-10-11 18:23:35 -0500

    Add all tutorial assets

--- lib/screens/tutorialscreen.dart
+++ lib/screens/tutorialscreen.dart
@@ -21,22 +21,22 @@ class TutorialScreen extends StatefulWidget {
         title: 'What is Scooping?',
         description:
             'You can use a technique called scooping to drastically improve your speed! You can scoop by starting to drag at any place on the screen, and then dragging over someone to scoop them up!',
-        display: AssetImage('assets/tutorial/step1.gif')),
+        display: AssetImage('assets/tutorial/step2.gif')),
     Step(
         title: 'Who are those Mask Guys?',
         description:
             'If a person is wearing a mask, then they are infected! Infected people will infect others if they get too close! Don\'t let them in the houses!',
-        display: AssetImage('assets/tutorial/step1.gif')),
+        display: AssetImage('assets/tutorial/step3.gif')),
     Step(
         title: 'What is that Thing at the Top?',
         description:
             'That is the hospital! Drag infected patients in, and after they are better you can drag them back out! But be careful you can only fit 3 people in at a time!',
-        display: AssetImage('assets/tutorial/step1.gif')),
+        display: AssetImage('assets/tutorial/step4.gif')),
     Step(
         title: 'How do I Win?',
         description:
             'You get a point for putting a person in the right house, and lose a point for putting them in the wrong one. If someone infected get\'s in a house, it will loose all of it\'s points!',
-        display: AssetImage('assets/tutorial/step1.gif')),
+        display: AssetImage('assets/tutorial/step5.gif')),
   ];
 
   @override

33f3da54d34929d48f01c3a997ebe9353c5e73d5
Macuyler <macuyler@gmail.com>
2020-10-15 17:44:45 -0500

    Show test add in debug mode

--- lib/ui/ads.dart
+++ lib/ui/ads.dart
@@ -4,6 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:firebase_admob/firebase_admob.dart';
 
 class Ads extends StatefulWidget {
+  bool get isInDebugMode {
+    bool inDebugMode = false;
+    assert(inDebugMode = true);
+    return inDebugMode;
+  }
+
   @override
   _AdsState createState() => _AdsState();
 }
@@ -16,21 +22,34 @@ class _AdsState extends State<Ads> {
     testDevices: <String>[],
   );
 
-  InterstitialAd myInterstitial = InterstitialAd(
-    adUnitId: Platform.isAndroid
-        ? "ca-app-pub-4873890200560512/8350729783"
-        : "ca-app-pub-4873890200560512/6702676001",
-    targetingInfo: targetingInfo,
-    listener: (MobileAdEvent event) {
-      print("InterstitialAd event is $event");
-    },
-  );
+  String unitId = "ca-app-pub-3940256099942544/1033173712"; // Test ID
+
+  void _setUnitId() {
+    if (!widget.isInDebugMode) {
+      if (Platform.isAndroid) {
+        this.unitId = "ca-app-pub-4873890200560512/8350729783"; // Android ID
+      } else if (Platform.isIOS) {
+        this.unitId = "ca-app-pub-4873890200560512/6702676001"; // iOS ID
+      }
+    }
+  }
+
+  InterstitialAd getAd() {
+    return InterstitialAd(
+      adUnitId: this.unitId,
+      targetingInfo: targetingInfo,
+      listener: (MobileAdEvent event) {
+        print("InterstitialAd event is $event");
+      },
+    );
+  }
 
   @override
   void initState() {
+    _setUnitId();
     FirebaseAdMob.instance
         .initialize(appId: "ca-app-pub-4873890200560512~5916138138");
-    myInterstitial
+    getAd()
       ..load()
       ..show();
     super.initState();

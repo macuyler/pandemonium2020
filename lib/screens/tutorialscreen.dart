2608ba3951560d248df2dc27717de258c73a25cf
Macuyler <macuyler@gmail.com>
2020-10-17 09:42:45 -0500

    Add tutorial censorship logic

--- lib/screens/tutorialscreen.dart
+++ lib/screens/tutorialscreen.dart
@@ -1,10 +1,13 @@
+import 'dart:io' show Platform;
+
 import 'package:flutter/material.dart';
 
 class Step {
   final String title;
   final String description;
+  final String censoredDesc;
   final AssetImage display;
-  Step({this.title, this.description, this.display});
+  Step({this.title, this.description, this.censoredDesc, this.display});
 }
 
 class TutorialScreen extends StatefulWidget {
@@ -16,25 +19,35 @@ class TutorialScreen extends StatefulWidget {
         title: 'How Do I Play?',
         description:
             'The object of the game is to get as many points as possible by dragging people to the house that matches their color to save them from the Pandemonium!',
+        censoredDesc:
+            'The object of the game is to get as many points as possible by dragging people to the house that matches their color to save them from the Pandemonium!',
         display: AssetImage('assets/tutorial/step1.gif')),
     Step(
         title: 'What is Scooping?',
         description:
+            'You can use a technique called scooping to drastically improve your speed! You can scoop by starting to drag at any place on the screen, and then dragging over someone to scoop them up!',
+        censoredDesc:
             'You can use a technique called scooping to drastically improve your speed! You can scoop by starting to drag at any place on the screen, and then dragging over someone to scoop them up!',
         display: AssetImage('assets/tutorial/step2.gif')),
     Step(
         title: 'Who are those Mask Guys?',
         description:
             'If a person is wearing a mask, then they are infected! Infected people will infect others if they get too close! Don\'t let them in the houses!',
+        censoredDesc:
+            'If a person is wearing a mask, then they are infected! Infected people will infect others if they get too close! Don\'t let them in the houses!',
         display: AssetImage('assets/tutorial/step3.gif')),
     Step(
         title: 'What is that Thing at the Top?',
         description:
             'That is the hospital! Drag infected patients in, and after they are better you can drag them back out! But be careful you can only fit 3 people in at a time!',
+        censoredDesc:
+            'That is the hospital! Drag infected patients in, and after they are better you can drag them back out! But be careful you can only fit 3 people in at a time!',
         display: AssetImage('assets/tutorial/step4.gif')),
     Step(
         title: 'How do I Win?',
         description:
+            'You get a point for putting a person in the right house, and lose a point for putting them in the wrong one. If someone infected get\'s in a house, it will loose all of it\'s points!',
+        censoredDesc:
             'You get a point for putting a person in the right house, and lose a point for putting them in the wrong one. If someone infected get\'s in a house, it will loose all of it\'s points!',
         display: AssetImage('assets/tutorial/step5.gif')),
   ];
@@ -80,7 +93,10 @@ class _TutorialScreenState extends State<TutorialScreen> {
                             height: MediaQuery.of(context).size.width - 40)),
                     Padding(
                       padding: EdgeInsets.fromLTRB(12, 20, 12, 10),
-                      child: Text(widget.steps[_step].description,
+                      child: Text(
+                          Platform.isIOS
+                              ? widget.steps[_step].censoredDesc
+                              : widget.steps[_step].description,
                           style: TextStyle(color: Colors.white, fontSize: 20)),
                     ),
                   ],

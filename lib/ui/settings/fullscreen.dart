f1c0ecfa4b68f8d49ed72f4a716c26cb6fb0a683
Macuyler <macuyler@gmail.com>
2020-11-29 09:17:07 -0600

    Add fullscreen toggle

--- lib/ui/settings/fullscreen.dart
+++ lib/ui/settings/fullscreen.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
-class FullScreen extends StatelessWidget {
+class FullScreen extends StatefulWidget {
   FullScreen({Key key}) : super(key: key);
+
+  @override
+  _FullScreenState createState() => _FullScreenState();
+}
+
+class _FullScreenState extends State<FullScreen> {
+  bool _useFullScreen = true;
+
+  void handleChange(bool value) {
+    setState(() {
+      _useFullScreen = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +32,21 @@ class FullScreen extends StatelessWidget {
               'This prevents accidental gesture inputs and button presses while playing.',
               style: TextStyle(color: Colors.white30, fontSize: 12)),
         ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Switch(
+              value: _useFullScreen,
+              onChanged: handleChange,
+              inactiveTrackColor: Colors.white10,
+            ),
+            Text('Enable Full Screen',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400)),
+          ],
+        )
       ],
     );
   }
